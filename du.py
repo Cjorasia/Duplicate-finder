65a1ef0ec7461a6a93e79f78ab9790891b1b78df
cjorasia <59288893+Cjorasia@users.noreply.github.com>
2020-06-09 15:00:56 +0530

    Initial commit

---
+++ du.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+path=input("enter path")
+unique = dict()
+
+if(os.path.exists(path)==False):
+    print("Error: path not found")
+
+for path,folders,files in os.walk(path):
+    for filename in files:
+        if filename not in unique:
+            unique[filename] = filename
+        else:
+            print(filename + ' is a duplicate of ' + unique[filename])
+            file_path = os.path.join(path, filename)
+            print(file_path)
+            source= file_path
+            destination='Documents'
+            print(path)
+            shutil.move(source,destination)
+
+
+    
